19a275e92ef19af4978f8b713e0c7490276ea19a
kryseno <kryseno@gmail.com>
2018-01-23 16:20:56 -0800

    [added] js oop file, edited script src to enable oop.js for now

---
+++ oop.js
@@ -0,0 +1,19 @@
+$(document).ready(initializeApp);
+
+function initializeApp() {
+
+}
+
+function MemoryMatchGame() {
+    this.cards = [];
+    this.imageList = [];
+    this.initializeGame = function() {
+
+    }
+    this.createCards = function() {
+
+    }
+    this.handleCardClick = function() {
+        
+    }
+}
