f69f00fc41de26b08ecd8422d31f94583ea61e95
kryseno <kryseno@gmail.com>
2018-01-23 17:59:07 -0800

    [added] new Card functionality. creates card; [updated] this.createCards func. appends card to game-area

---
+++ includes/card.js
@@ -0,0 +1,33 @@
+/*
+<div class="card">
+    <div class="front">
+        <img src="images/michael.jpg">
+    </div>
+    <div class="back">
+        <img src="images/dunderMifflin.png">
+    </div>
+</div>
+*/
+
+function Card(frontImg, parentObj) {
+    this.frontImg = frontImg;
+    this.parentObj = parentObj;
+    this.revealed = false;
+
+    this.render = function(){
+        var card = $("<div>", {
+            class: 'card'
+        });
+        var front = $("<div>", {
+            class: 'front'
+        });
+        var imgSrc = $("<img>", {
+            src: this.frontImg
+        });
+        var back = $("<div>", {
+            class: 'back'
+        });
+        card.append(front, back);
+        return card;
+    }
+}
