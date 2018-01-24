bfc030ea5d571f34a98d75cfb6f71cd59fa89d2f
kryseno <kryseno@gmail.com>
2018-01-23 16:31:15 -0800

    [added] array of total images in this.initializeGame(), began this.createCards function

--- oop.js
+++ oop.js
@@ -8,12 +8,24 @@ function initializeApp() {
 
 function MemoryMatchGame() {
     this.cards = [];
-    this.imageList = [];
+    this.imageList = [
+        'images/andy.jpg',
+        'images/dwight.jpg',
+        'images/gabe.jpg',
+        'images/jim.jpg',
+        'images/kevin.jpg',
+        'images/michael.jpg',
+        'images/pam.jpg',
+        'images/stanley.jpg',
+        'images/toby.jpg'
+    ];
     this.initializeGame = function() {
-
+        const images = this.imageList.concat(this.imageList);
+        console.log('take imageList & added itself onto it (doubling it)');
+        this.createCards(images);
     }
     this.createCards = function() {
-
+        
     }
     this.handleCardClick = function() {
         
