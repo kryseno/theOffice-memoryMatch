f6f4e276efce70a77598695b3e3fea871f58a0f9
kryseno <kryseno@gmail.com>
2018-02-14 14:14:28 -0800

    [added] loader

--- scripts/oop.js
+++ scripts/oop.js
@@ -4,4 +4,15 @@ var game = null;
 function initializeApp() {
     game = new MemoryMatchGame();
     game.initializeGame();
+    loader();
+}
+
+function loader() {
+    var handleLoader;
+    handleloader = setTimeout(showContent, 3000);
+}
+
+function showContent() {
+    $("#loader").css("display", "none");
+    $(".container").css("display", "block");
 }
